a0b9f93f3f7827a5af2710d44188f9e3070ff652
Pedro Galocha <pedrogalocha@gmail.com>
2019-09-29 20:07:39 -0300

    Alteração envio de email.

--- application/config/email.php
+++ application/config/email.php
@@ -2,10 +2,10 @@
 
 $config = array(
     'protocol' => 'smtp',
-    'smtp_host' => 'smtp.sendgrid.net',
-    'smtp_user' => 'pedrogalocha',
+    'smtp_host' => 'a2plcpnl0842.prod.iad2.secureserver.net',
+    'smtp_user' => 'contato@laviezoo.com.br',
     'smtp_pass' => '631542aW1',
-    'smtp_port' => 587,
+    'smtp_port' => 465,
     'crlf' => "\r\n",
     'newline' => "\r\n"
 );
