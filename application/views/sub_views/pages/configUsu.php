ec260319ea762d9a23c29189b58421fd0e81ef50
Pedro Galocha <pedrogalocha@gmail.com>
2019-10-15 22:35:02 -0300

    Tirando print

--- application/views/sub_views/pages/configUsu.php
+++ application/views/sub_views/pages/configUsu.php
@@ -7,7 +7,7 @@
         <div class="rounded px-2 pt-3 pb-5 align-items-center justify-content-center"
           style="background-color: rgba(255, 255, 255, 0.85)">
           <!-- Começo da border transparente-->
-          <?php  print_r($userInfo['USUARIO_NIVEL_ACESSO']) ;?>
+          
           <?php if(strpos($userInfo['USUARIO_NIVEL_ACESSO'], "CLI") !== false ) {?> <img class=""
             src="<?php echo base_url('includes/img/agencia_cadastro_icone.svg') ?>" style="
       position:absolute;
