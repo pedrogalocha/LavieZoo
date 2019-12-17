2c5fc9aef9a4b4ee40cf576156ee4e2d9394d4db
paulo <paulo041299@gmail.com>
2019-12-17 20:21:40 -0300

    teste pdf

--- application/views/sub_views/pages/teste_pdf.php
+++ application/views/sub_views/pages/teste_pdf.php
@@ -1,31 +1,219 @@
-<?php
 
-require_once 'vendor/autoload.php';
 
-// referenciando o namespace do dompdf
 
-use Dompdf\Dompdf;
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 
-// instanciando o dompdf
+<title>PDF LAVIEZOO</title>
+<style type="text/css">
 
-$dompdf = new Dompdf();
+@page {
+	margin: 2cm;
+}
 
-//lendo o arquivo HTML correspondente
+body {
+  font-family: sans-serif;
+	margin: 0.5cm 0;
+	text-align: justify;
+}
 
-$html = file_get_contents('application/views/sub_views/pages/exemplo.php');
+#header,
+#footer {
+  position: fixed;
+  left: 0;
+	right: 0;
+	color: #aaa;
+	font-size: 0.9em;
+}
 
-//inserindo o HTML que queremos converter
+#header {
+  top: 0;
+	border-bottom: 0.1pt solid #aaa;
+}
 
-$dompdf->loadHtml($html);
+#footer {
+  bottom: 0;
+  border-top: 0.1pt solid #aaa;
+}
 
-// Definindo o papel e a orientação
+#header table,
+#footer table {
+	width: 100%;
+	border-collapse: collapse;
+	border: none;
+}
 
-$dompdf->setPaper('A4', 'landscape');
+#header td,
+#footer td {
+  padding: 0;
+	width: 50%;
+}
 
-// Renderizando o HTML como PDF
+.page-number {
+  text-align: center;
+}
 
-$dompdf->render();
+.page-number:before {
+  content: "Page " counter(page);
+}
 
-// Enviando o PDF para o browser
+hr {
+  page-break-after: always;
+  border: 0;
+}
 
-$dompdf->stream();
+</style>
+  
+</head>
+
+<body>
+
+<div id="header">
+  <table>
+    <tr>
+      <td>Example document</td>
+      <td style="text-align: right;">Author</td>
+    </tr>
+  </table>
+</div>
+
+<div id="footer">
+  <div class="page-number"></div>
+</div>
+
+<h2>Section 1</h2>
+
+<p>
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed non
+risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
+ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula
+massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci
+nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit
+amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat
+in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero
+pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo
+in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue
+blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus
+et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed
+pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales
+hendrerit.</p>
+
+<hr/>
+
+<h2>Section 2</h2>
+
+<p>
+	Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut
+orci vel massa suscipit pulvinar. Nulla sollicitudin. Fusce varius,
+ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus
+sapien eros vitae ligula. Pellentesque rhoncus nunc et augue. Integer
+id felis. Curabitur aliquet pellentesque diam. Integer quis metus vitae
+elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer
+adipiscing elit. Morbi vel erat non mauris convallis vehicula. Nulla et
+sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue
+eu, quam. Mauris ullamcorper felis vitae erat. Proin feugiat, augue non
+elementum posuere, metus purus iaculis lectus, et tristique ligula
+justo vitae magna. Morbi vel erat non mauris convallis vehicula. Nulla et
+sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue
+eu, quam. Mauris ullamcorper felis vitae erat. Proin feugiat, augue non
+elementum posuere, metus purus iaculis lectus, et tristique ligula
+justo vitae magna.</p>
+
+<hr/>
+
+<h2>Section 3</h2>
+
+<!-- yeah, we'll have to do better for inline floating elements -->
+<p><span style="float: left; font-size: 4em; width: 0.7em; height: 0.9em; line-height: 1;">A</span>liquam convallis sollicitudin purus. Praesent aliquam, enim at
+fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu
+lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod
+libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean
+suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla
+tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+felis magna fermentum augue, et ultricies lacus lorem varius purus.
+Curabitur eu amet.</p>
+
+<p>Aliquam convallis sollicitudin purus. Praesent aliquam, enim at
+fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu
+lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod
+libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean
+suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla
+tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+felis magna fermentum augue, et ultricies lacus lorem varius purus.
+Curabitur eu amet.</p>
+
+<p>Aliquam convallis sollicitudin purus. Praesent aliquam, enim at
+fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu
+lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod
+libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean
+suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla
+tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+felis magna fermentum augue, et ultricies lacus lorem varius purus.
+Curabitur eu amet.</p>
+
+<p>Aliquam convallis sollicitudin purus. Praesent aliquam, enim at
+fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu
+lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod
+libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean
+suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla
+tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+felis magna fermentum augue, et ultricies lacus lorem varius purus.
+Curabitur eu amet.</p>
+
+<p>Aliquam convallis sollicitudin purus. Praesent aliquam, enim at
+fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu
+lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod
+libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean
+suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla
+tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+felis magna fermentum augue, et ultricies lacus lorem varius purus.
+Curabitur eu amet.</p>
+
+<p>Aliquam convallis sollicitudin purus. Praesent aliquam, enim at
+fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu
+lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod
+libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean
+suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla
+tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+felis magna fermentum augue, et ultricies lacus lorem varius purus.
+Curabitur eu amet.</p>
+
+<p>Aliquam convallis sollicitudin purus. Praesent aliquam, enim at
+fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu
+lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod
+libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean
+suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla
+tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+felis magna fermentum augue, et ultricies lacus lorem varius purus.
+Curabitur eu amet.</p>
+
+<p>Aliquam convallis sollicitudin purus. Praesent aliquam, enim at
+fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu
+lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod
+libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean
+suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla
+tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+felis magna fermentum augue, et ultricies lacus lorem varius purus.
+Curabitur eu amet.</p>
+
+<p>Aliquam convallis sollicitudin purus. Praesent aliquam, enim at
+fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu
+lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod
+libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean
+suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla
+tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+felis magna fermentum augue, et ultricies lacus lorem varius purus.
+Curabitur eu amet.</p>
+
+<p>Aliquam convallis sollicitudin purus. Praesent aliquam, enim at
+fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu
+lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod
+libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean
+suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla
+tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+felis magna fermentum augue, et ultricies lacus lorem varius purus.
+Curabitur eu amet.</p>
+
+</body></html>
